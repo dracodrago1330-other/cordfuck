1f3e12c2ede1352622e478a0060bf796c5b61d2f
VixieTSQ <62594135+VixieTSQ@users.noreply.github.com>
2021-10-23 15:31:57 -0600

    Small commit for the sake of testing CD

--- index.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-    $("div.center").scrollTop( 200 );
-               
-    $("div.center").mousemove(function () {
-       var top = $("div.center").scrollTop();
-       $("#result").html("top offset: <span>" + top + "</span>.");
-    });
-});
